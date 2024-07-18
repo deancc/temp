f2a03eee46e55c554803cd460bd926030d08dc1e
deancc <deancc@outlook.com>
2024-07-19 00:59:45 +0200

    Update Connection_Monitor.ps1

--- Connection_Monitor.ps1
+++ Connection_Monitor.ps1
@@ -1,80 +1,128 @@
-$str_time = Get-Date -Format "dd-MM-yyyy"
+#
+# string[] to string _or no return
+function StringBuilder()
+{
+    param( [string[]] $ParameterName )
+    
+    foreach ($Parameter in $ParameterName)
+    {
+        [string] $Result += $Parameter
+    }
+    
+    if(![string]::IsNullOrEmpty($Result))
+    {
+        return $Result
+    }
+}
 
-$ui_dots = 10
+function ParseDestinationList()
+{
 
-$path = (Get-Location).Path
+}
 
-$lan_d = (Get-NetRoute "0.0.0.0/0" | Select-Object -Property NextHop).NextHop
+function TestDestination()
+{
 
-$wan_d = "8.8.8.8"
+}
 
-$log_path = "$path" + "\logs\"
+# Variable Declarations
+[string]$time = Get-Date -Format "dd-MM-yyyy"
 
-$log = New-Item "$path" + "\ping_log_($string_time).txt" -Force
+[string]$log_name = "connection_log"
 
+[string]$log_directory = "\logs\"
+
+[string]$current_directory = (Get-Location).Path
+
+[string]$log_path = StringBuilder($current_directory, $log_directory)
+
+[string]$wan_destination = "8.8.8.8"
+
+[string]$lan_destination = (Get-NetRoute "0.0.0.0/0" | Select-Object -Property NextHop).NextHop
+
+[string]$prompt_info = "CTRL+C to Exit"
+
+[string]$prompt_wan = "Input alternate WAN? [default] $([string]$wan_destination)"
+
+[string]$prompt_lan = "Input alternate LAN? [default] $([string]$lan_destination)"
+
+[string]$prompt_delay = "Request delay in seconds?"
+
+[int]$request_delay = 10
+
+[int]$ui_dots = $request_delay * 2
+
+# Validate / Generate Log Path and Log File
 If(!(test-path -PathType container $log_path))
 {
       New-Item -ItemType Directory -Path $log_path
 }
 
-clear
+$log = New-Item (StringBuilder($current_directory, $log_directory, $log_name, "_", $time, ".txt")) -Force
 
-Write-Host("CTRL+C to Exit")
+Clear-Host
 
-$wan_d_alt = Read-Host("Input alternate WAN address? [default (google) $wan_d] ")
-$lan_d_alt = Read-Host("Input alternate LAN address? [default (default gateway) $lan_d] ")
-$req_delay = Read-Host("Delay request by num second? [default $($dots / 2)] ")
+# User Prompt and Inpu
+Write-Host($prompt_info)
+$wan_destination_alt = Read-Host($prompt_wan)
+$lan_destination_alt = Read-Host($prompt_lan)
+$request_delay = Read-Host($prompt_delay)
 
-if($wan_d_alt)
+#
+# User Input (kinda) Validation
+if(![string]::IsNullOrEmpty($wan_destination_alt))
 {
-    $wan_d = $wan_d_alt
+    if(Test-Connection $wan_destination_alt)
+    {
+        $wan_destination = $wan_destination_alt
+    }
 }
 
-if($lan_d_alt)
+if(![string]::IsNullOrEmpty($lan_destination_alt))
 {
-    $lan_d = $lan_d_alt
+    if(Test-Connection $lan_destination_alt)
+    {
+        $lan_destination = $lan_destination_alt
+    }
 }
 
-if($req_delay -and (($req_delay / 2.0) -gt 1))
+if($request_delay -ge 1)
 {
-    $ui_dots = $req_delay / 0.5
+    [int]$ui_dots = ([int]$request_delay) * 2
 }
+else { $request_delay = 10 }
 
-clear
+Clear-Host
 
+# Test Connections Loop
 while($true)
 {
-    if(!(Test-Connection -ComputerName $wan_d -Count 1 -Quiet))
-    {
-        $log_cont = "Unreachable;$wan_d;$(Get-Date -Format 'u')"
-        Write-Host ("$log_cont")
-        Add-Content $log -value $log_cont
-    }
-    
-    else
-    {
-        Write-Host("Response;$wan_d")
-    }
-    
-    
-    if(!(Test-Connection -ComputerName $lan_d -Count 1 -Quiet))
-    {
-        $log_cont = "Unreachable;$lan_d;$(Get-Date -Format 'u')"
-        Write-Host ("$log_cont")
-        Add-Content $log -value $log_cont
-    }
-    
-    else
-    {
-        Write-Host("Response;$lan_d")
-    }
+    Write-Host(StringBuilder("Log Directory: ", $log_path, "`n"))
 
-    for($dot_iter = 0; $dot_iter -lt $ui_dots; $dot_iter++)
+    if(!(Test-Connection -ComputerName $wan_destination -Count 1 -Quiet))
+    {
+        $log_content = StringBuilder("Unreachable", ";", $wan_destination, ";", $(Get-Date))
+        Write-Host ("$log_content")
+        Add-Content $log -value $log_content
+    }
+    else{ Write-Host("Response;$wan_destination") }
+    
+    if(!(Test-Connection -ComputerName $lan_destination -Count 1 -Quiet))
+    {
+        $log_content = StringBuilder("Unreachable", ";", $lan_destination, ";", $(Get-Date))
+        Write-Host ("$log_content")
+        Add-Content $log -value $log_content
+    } 
+    else{ Write-Host("Response;$lan_destination") }
+
+    # Display Output and Progress
+    Write-Host("[$($request_delay)s]|") -NoNewline
+
+    for($i = 0; $i -lt $ui_dots; $i++)
     {
         Write-Host(".") -NoNewline
         Start-Sleep -Milliseconds 500
     }
 
-    clear
-    
+    Clear-Host
 }
